e41e8dc851a9bf6351a7493d81fba2c886dc50ee
Farid <fargmov@gmail.com>
2022-12-22 22:21:40 +0300

    add transactions page

---
+++ src/pages/Transactions.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Transactions = () => {
+    return (
+        <div
+            style={{
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <p>Coming Soon...</p>
+        </div>
+    )
+}
+
+export default Transactions
