d0a66226c698fdeb2a7c9e6d571fc5f7d793a67c
Farid <fargmov@gmail.com>
2023-02-27 20:29:33 +0300

    add delete product mutation

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,11 @@
-import { styled } from '@mui/material'
+import { CircularProgress, styled } from '@mui/material'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Delete from '@mui/icons-material/Delete'
 import Edit from '@mui/icons-material/Edit'
+import { useDeleteProductMutation } from '../redux/adminApi/adminApi'
+import LoadingContainer from './LoadingContainer'
 // import { Box } from '@mui/system'
 
 const Product = styled(Box)({
@@ -30,60 +32,78 @@ const StyledButton = styled(Button)({
 })
 
 const ProductItem = ({ product }) => {
+    const [deleteProduct, { isLoading: isDeleting }] =
+        useDeleteProductMutation()
+
     return (
-        <Product>
-            <Box sx={{ height: '65%', borderBottom: '1px solid #9a9a9a' }}>
-                <img
-                    style={{
-                        height: '100%',
-                        width: '100%',
-                        objectFit: 'cover',
-                    }}
-                    src={product.img[1]}
-                    alt="t-shirt"
-                />
-            </Box>
-            <Box sx={{ height: '35%', padding: '10px' }}>
-                <InfoContainer>
-                    <Typography
+        <>
+            {isDeleting ? (
+                <LoadingContainer>
+                    <CircularProgress />
+                </LoadingContainer>
+            ) : (
+                <Product>
+                    <Box
                         sx={{
-                            fontSize: '15px',
-                            fontWeight: '400',
-                            marginBottom: '5px',
+                            height: '65%',
+                            borderBottom: '1px solid #9a9a9a',
                         }}
-                        variant="span"
                     >
-                        {product.title.slice(0, 17) + '...'}
-                    </Typography>
-                    <Typography
-                        sx={{
-                            fontSize: '16px',
-                            fontWeight: '600',
-                            color: '#000',
-                        }}
-                        variant="span"
-                    >
-                        $ {product.price}
-                    </Typography>
-                </InfoContainer>
-                <ButtonContainer>
-                    <StyledButton
-                        sx={{ marginRight: '6px' }}
-                        variant="outlined"
-                        startIcon={<Edit />}
-                    >
-                        Edit
-                    </StyledButton>
-                    <StyledButton
-                        variant="outlined"
-                        color="error"
-                        startIcon={<Delete />}
-                    >
-                        Delete
-                    </StyledButton>
-                </ButtonContainer>
-            </Box>
-        </Product>
+                        <img
+                            style={{
+                                height: '100%',
+                                width: '100%',
+                                objectFit: 'cover',
+                            }}
+                            src={product.img[1]}
+                            alt="t-shirt"
+                        />
+                    </Box>
+                    <Box sx={{ height: '35%', padding: '10px' }}>
+                        <InfoContainer>
+                            <Typography
+                                sx={{
+                                    fontSize: '15px',
+                                    fontWeight: '400',
+                                    marginBottom: '5px',
+                                }}
+                                variant="span"
+                            >
+                                {product.title.slice(0, 17) + '...'}
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontSize: '16px',
+                                    fontWeight: '600',
+                                    color: '#000',
+                                }}
+                                variant="span"
+                            >
+                                $ {product.price}
+                            </Typography>
+                        </InfoContainer>
+                        <ButtonContainer>
+                            <StyledButton
+                                sx={{ marginRight: '6px' }}
+                                variant="outlined"
+                                startIcon={<Edit />}
+                            >
+                                Edit
+                            </StyledButton>
+                            <StyledButton
+                                variant="outlined"
+                                color="error"
+                                startIcon={<Delete />}
+                                disabled={isDeleting}
+                                onClick={() => deleteProduct(product._id)}
+                            >
+                                Delete
+                            </StyledButton>
+                        </ButtonContainer>
+                    </Box>
+                </Product>
+            )}
+        </>
     )
 }
 
